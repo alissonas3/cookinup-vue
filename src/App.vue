0e862990bb35c75184d9f6f49dc50493cba01a37
Alisson Alves <92827772+alissonas3@users.noreply.github.com>
2024-01-26 10:42:08 -0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+
+<template>
+  <Banner />
+  <MainContent />
+  
+</template>
+
+<script lang="ts">
+import Banner from "./components/Banner.vue";
+import MainContent from './components/MainContent.vue';
+
+  export default {
+    name: "App",
+    components: {
+      Banner,
+      MainContent
+    }
+}
+</script>
+
+<style scoped>
+</style>
